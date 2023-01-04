417d09358c652a325f85170967e0ddd1252c3e09
Hung Nguyen <ndgnuh@protonmail.com>
2023-01-04 08:40:46 +0700

    box blur

--- black_trdg/transforms.py
+++ black_trdg/transforms.py
@@ -15,6 +15,11 @@ def gaussian_blur(image, radius):
     return image.filter(kernel)
 
 
+def box_blur(image, radius, Filter):
+    kernel = ImageFilter.BoxBlur(radius=radius)
+    return image.filter(kernel)
+
+
 @dataclass
 class RandomGaussianBlur:
     min_radius: float = 1
@@ -23,6 +28,16 @@ class RandomGaussianBlur:
     def __call__(self, image):
         radius = random.random(np.arange(min_radius, max_radius, 0.01))
         return gaussian_blur(radius, radius)
+
+
+@dataclass
+class RandomBoxBlur:
+    min_radius: float = 1
+    max_radius: float = 3
+
+    def __call__(self, image):
+        radius = random.random(np.arange(min_radius, max_radius, 0.01))
+        return box_blur(radius, radius)
 
 
 @dataclass
